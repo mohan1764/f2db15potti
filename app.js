9a2746704592b79138f1bcbf18675c9222c44be9
mohan1764 <s554815@nwmissouri.edu>
2022-11-18 00:22:55 -0600

    added

--- app.js
+++ app.js
@@ -3,6 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+   Account.findOne({ username: username }, function (err, user) {
+     if (err) { return done(err); }
+     if (!user) {
+       return done(null, false, { message: 'Incorrect username.' });
+     }
+     if (!user.validPassword(password)) {
+       return done(null, false, { message: 'Incorrect password.' });
+     }
+     return done(null, user);
+   });
+  }));
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+    }));
+var Account =require('./models/account'));
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 require('dotenv').config();
 const connectionString =
 process.env.MONGO_CON
@@ -23,6 +49,7 @@ var gridbuildRouter = require('./routes/gridbuild');
 var selectorRouter = require('./routes/selector');
 var resourceRouter = require('./routes/resource');
 var elephant = require("./models/elephant");
+const { default: mongoose } = require('mongoose');
 var app = express();
 
 async function recreateDB(){
